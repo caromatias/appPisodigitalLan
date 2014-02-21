edf83c2e73bfafeb0e55225c6755e136b2895e79
Matías Caro Serrano <caromatiasm@gmail.com>
2014-02-21 15:20:00 -0300

    AGREGA PANEL

--- gen/com/caromatias/apppisodigitallan/R.java
+++ gen/com/caromatias/apppisodigitallan/R.java
@@ -26,24 +26,36 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int botoncomenzar=0x7f020000;
-        public static final int btn_azul=0x7f020001;
-        public static final int btn_comenzar=0x7f020002;
-        public static final int fondo_mapas=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo_splash=0x7f020005;
-        public static final int mapa_inter=0x7f020006;
+        public static final int boton=0x7f020000;
+        public static final int botoncomenzar=0x7f020001;
+        public static final int botongris=0x7f020002;
+        public static final int botonrojo=0x7f020003;
+        public static final int btn_azul=0x7f020004;
+        public static final int btn_comenzar=0x7f020005;
+        public static final int fondo_mapas=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int logo_splash=0x7f020008;
+        public static final int mapa_inter=0x7f020009;
+        public static final int panel_final=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0008;
-        public static final int btn_com=0x7f0a0005;
+        public static final int action_settings=0x7f0a0010;
+        public static final int btn_argentina=0x7f0a0004;
+        public static final int btn_brasil=0x7f0a0006;
+        public static final int btn_chile=0x7f0a0003;
+        public static final int btn_colombia=0x7f0a0008;
+        public static final int btn_com=0x7f0a000d;
+        public static final int btn_ecuador=0x7f0a0007;
+        public static final int btn_internacional=0x7f0a0009;
+        public static final int btn_peru=0x7f0a0005;
         public static final int img_fondo=0x7f0a0000;
         public static final int img_mapa=0x7f0a0001;
-        public static final int img_splashScreen=0x7f0a0006;
-        public static final int progressBarSplashScreen=0x7f0a0007;
-        public static final int relativeLayout1=0x7f0a0004;
-        public static final int videoPrincipal=0x7f0a0003;
-        public static final int video_lamina_uno=0x7f0a0002;
+        public static final int img_splashScreen=0x7f0a000e;
+        public static final int panel=0x7f0a0002;
+        public static final int progressBarSplashScreen=0x7f0a000f;
+        public static final int relativeLayout1=0x7f0a000c;
+        public static final int videoPrincipal=0x7f0a000b;
+        public static final int video_lamina_uno=0x7f0a000a;
     }
     public static final class layout {
         public static final int activity_lamina_uno=0x7f030000;
